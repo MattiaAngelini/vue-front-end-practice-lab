6bbeff2a01dcbbe60bd6a607a56832d10130cd93
MattiaAngelini <mattiaangelini1993@gmail.com>
2024-11-15 16:21:49 +0100

    Added: layout Header

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,40 @@
 <script lang="ts">
-
 import Header from './components/Header.vue'
 
 export default {
-
   components:{
     Header
+  },
+
+  data(){
+    return{
+
+      headerLinks:[
+          { text: 'Home', href: '/home' },
+          { text: 'Progetti', href: '/about' },
+          { text: 'Contatti', href: '/contact' },
+          { text: 'Linkedin', href: '/contact' },
+      ],
+      socialIcons:[
+          { logo: 'fa-brands fa-linkedin', href: 'https://www.linkedin.com/in/mattia-angelini-b00427311/' },
+          { logo: 'fa-brands fa-github', href: 'https://github.com/MattiaAngelini' },
+          
+      ],
+    }
   }
 }
-
 
 </script>
 
 <template>
 
-  <Header></Header>
+  <div class="ms-container">  
+    <Header :icons="socialIcons" :links="headerLinks"></Header>    
+  </div>
+
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+@use './assets/styles/generic.scss' as *;
 
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,19 +1,67 @@
 <script lang="ts">
-
 export default{
-  name: 'Header'
-}
+    name: 'Header',
+    props: {
 
+    links: {
+        type: Array as () => {text: string; href: string }[],
+        required: true,
+    },
+    icons: {
+        type: Array as () => {logo: string; href: string }[],
+        required: true,
+    },
+
+    },
+}
 </script>
 
 <template>
+  
+    <section class="p-3">
+       <!--Social Logo-->
+      <div class="icons-container d-flex justify-content-end"> 
+         <a :href="icon.href" v-for="icon in icons"> <i :class="icon.logo"></i></a> 
+      </div>
 
-
-  <div class="p-4">header</div>
-
+      <!--Header Links-->
+      <div class="links">
+        <img src="" alt="Logo">
+          <a v-for="link in links" :key="index" :href="link.href"> {{link.text}}</a>     
+      </div>  
+    </section>
 
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+@use '../assets/styles/generic.scss' as *;
+
+  section{
+
+    background-color: $secondary-color;
+
+    a{
+      text-decoration: none;
+      color: $main-color;
+    }  
+
+    .links{
+      display: grid;
+      grid-template-columns: 2fr repeat(4,1fr);
+    }
+    
+    .icons-container > a{
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      height: 30px;
+      width: 30px;
+      margin-right: 2%;
+      background-color: white;
+      padding: 4px;
+      border-radius: 100%;
+    }
+
+  }
 
 </style>
