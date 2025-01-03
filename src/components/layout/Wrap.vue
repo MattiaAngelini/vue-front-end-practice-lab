bfc9d73dbcfa5336137e44d81ce664a7664036c9
MattiaAngelini <mattiaangelini1993@gmail.com>
2025-01-03 21:06:04 +0100

    Fixed wrapper

--- src/components/layout/Wrap.vue
+++ src/components/layout/Wrap.vue
@@ -22,10 +22,9 @@ export default{
 }
 </script>
 <template>  
+
     <div>
-       
         <img :src= wrap alt="">
-  
         <div :style="shadow" class="shadow"></div>
     </div>
 </template>
@@ -34,13 +33,16 @@ export default{
 @use '../../assets/styles/generic.scss' as *;
 
 div{
- 
+    min-width: 100%;
     position: fixed;
     bottom: 0;
     z-index: -1;
-    min-height: 100vh;
     
-
+    img{
+        width: 100%;
+        min-height: 100vh;
+    }   
+    
     .shadow{
         content: '';
         width: 100%;
